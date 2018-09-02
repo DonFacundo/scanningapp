3e0b4d58bbf3a48025cd7e6642e70c8e33ddd20e
DonFacundo <gedwynedeleon@gmail.com>
2018-09-03 07:58:01 +1200

    *** (for later) Firebase and scan qr changes

--- App.js
+++ App.js
@@ -84,3 +84,101 @@ const styles = StyleSheet.create({
 
 
 AppRegistry.registerComponent('BadInstagramCloneApp', () => BadInstagramCloneApp);
+
+// import React, { Component } from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View,
+//   AppRegistry,
+//   Dimensions,
+//   TouchableOpacity,
+//   Linking
+// } from 'react-native';
+// import { RNCamera } from 'react-native-camera';
+// import QRCodeScanner from 'react-native-qrcode-scanner';
+// // import firebase from 'react-native-firebase';
+// import { db } from './Firebase/firebase.js';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+// type Props = {};
+// let scanner;
+
+// export default class App extends Component<Props> {
+//     constructor(props) {
+//       super(props);
+
+//       this.state = {
+//                       code: 'tit3'
+//                   };
+//     } 
+//    onSuccess(e) {
+//     // Linking
+//     //   .openURL(e.data)
+//     //   .catch(err => console.error('An error occured', err));
+//     const qrcode = e.data;
+
+//     this.setState({ code: qrcode });
+//     scanner.reactivate();
+//   }
+//   render() {
+//     const {code} = this.state;
+//     const startScan = () => {
+//       if (scanner) {
+//         scanner.reactivate();
+//       }
+//     };
+//     return (
+//       <View style={styles.container}>
+//         <QRCodeScanner
+//         ref={(camera) => scanner = camera}
+//         onRead={this.onSuccess.bind(this)}
+//         topContent={
+//           <Text style={styles.centerText}>
+//             Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+//           </Text>
+//         }
+//         bottomContent={
+//           <TouchableOpacity style={styles.buttonTouchable} onPress={() => startScan()}>
+//             <Text style={styles.buttonText}>CODE: {code}</Text>
+//           </TouchableOpacity>
+//         }
+//       />
+//       </View>
+//     );
+//   }
+// }
+  
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: 'white'
+//   },
+//   centerText: {
+//     flex: 1,
+//     fontSize: 18,
+//     padding: 32,
+//     color: '#777',
+//   },
+//   textBold: {
+//     fontWeight: '500',
+//     color: '#000',
+//   },
+//   buttonText: {
+//     fontSize: 21,
+//     color: 'rgb(0,122,255)',
+//   },
+//   buttonTouchable: {
+//     padding: 16,
+//   },
+// });
+
+// AppRegistry.registerComponent('default', () => ScanScreen);
---
+++ Firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+var config = {
+    apiKey: "AIzaSyCoStWpbcRjQGy_PPMvhU8VsC3jvfuoADM",
+    authDomain: "itickettest-462f1.firebaseapp.com",
+    databaseURL: "https://itickettest-462f1.firebaseio.com",
+    projectId: "itickettest-462f1",
+    storageBucket: "itickettest-462f1.appspot.com",
+    messagingSenderId: "414966053777"
+};
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+
+export const db = firestore;
